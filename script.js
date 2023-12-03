8b81a8c232e654b98335fec9e748bce7dc09ed5d
MIYA2021 <80123162+MIYA2021@users.noreply.github.com>
2023-12-04 00:53:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,77 +2,64 @@ const fallingObjectContainer = document.getElementById('falling-object-container
 const rotateButton = document.getElementById('rotate-button');
 const dropButton = document.getElementById('drop-button');
 let rotation = 0;
-let fallingObject = null;
 
 function createFallingObject() {
-  if (fallingObject) {
-    return;
-  }
-
-  fallingObject = document.createElement('div');
+  const fallingObject = document.createElement('div');
   fallingObject.className = 'falling-object';
 
+  // 新しい画像を表示するための img 要素を作成
   const fallingObjectImage = document.createElement('img');
-  fallingObjectImage.src = 'https://github.com/MIYA2021/siga.github.io/blob/main/new-object.png?raw=true'; // 新しい画像の相対パス
+  fallingObjectImage.src = 'https://github.com/MIYA2021/siga.github.io/blob/main/new-object.png?raw=true'; // 新しい画像のファイルパス
   fallingObjectImage.alt = 'falling object';
   fallingObject.appendChild(fallingObjectImage);
 
   fallingObjectContainer.appendChild(fallingObject);
 
   fallingObject.addEventListener('animationend', () => {
+    // アニメーションが終了したら、次の物を作成
+    createFallingObject();
+    // 現在の物を削除
     fallingObjectContainer.removeChild(fallingObject);
-    fallingObject = null;
   });
 
   return fallingObject;
 }
 
+// 初期物の作成
 createFallingObject();
 
 rotateButton.addEventListener('click', () => {
-  if (fallingObject) {
-    rotation += 15;
-    fallingObject.style.transform = `rotate(${rotation}deg)`;
-  }
+  // ボタンで回転
+  rotation += 15;
+  fallingObjectContainer.lastChild.style.transform = `rotate(${rotation}deg)`;
 });
 
 dropButton.addEventListener('click', () => {
-  if (fallingObject) {
-    fallingObject.style.animation = 'fall 2s linear forwards';
-    createFallingObject();
-  }
+  // ボタンで落とす
+  const fallingObject = createFallingObject(); // 新しいオブジェクトを作成
+  fallingObject.style.animation = 'fall 2s linear forwards';
 });
 
 document.addEventListener('dblclick', () => {
   // ダブルクリックでアニメーションをリセット
-  if (fallingObject) {
-    fallingObject.style.animation = '';
-  }
+  const fallingObject = fallingObjectContainer.lastChild;
+  fallingObject.style.animation = '';
 });
 
 document.addEventListener('touchstart', (e) => {
-  if (fallingObject) {
-    // タッチ開始時にオブジェクトの位置を設定
-    const touchY = e.touches[0].clientY;
-    fallingObject.style.top = `${touchY}px`;
-  }
+  const touchY = e.touches[0].clientY;
+  fallingObjectContainer.lastChild.style.top = `${touchY}px`;
 });
 
 document.addEventListener('touchmove', (e) => {
-  if (fallingObject) {
-    // タッチ中にオブジェクトの位置を更新
-    const touchY = e.touches[0].clientY;
-    fallingObject.style.top = `${touchY}px`;
-  }
+  const touchY = e.touches[0].clientY;
+  fallingObjectContainer.lastChild.style.top = `${touchY}px`;
 });
 
 document.addEventListener('touchend', () => {
   // ボタンで落とす
-  if (fallingObject) {
-    fallingObject.style.animation = 'fall 2s linear forwards';
-    // 新しいオブジェクトを作成
-    createFallingObject();
-  }
+  const fallingObject = createFallingObject(); // 新しいオブジェクトを作成
+  fallingObject.style.animation = 'fall 2s linear forwards';
 });
 
 rotateButton.addEventListener('touchstart', (e) => {
