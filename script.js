a5f067945e8ee3c8ed2cd9686b16c5df9c6e4bfd
MIYA2021 <80123162+MIYA2021@users.noreply.github.com>
2023-12-11 18:01:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,23 @@ function createFallingObject() {
   const fallingObject = document.createElement('div');
   fallingObject.className = 'falling-object';
 
-  // 新しい画像を表示するための img 要素を作成
   const fallingObjectImage = document.createElement('img');
-  fallingObjectImage.src = 'https://github.com/MIYA2021/siga.github.io/blob/main/new-object.png?raw=true'; // 新しい画像のファイルパス
+  fallingObjectImage.src = 'https://github.com/MIYA2021/siga.github.io/blob/main/new-object.png?raw=true';
   fallingObjectImage.alt = 'falling object';
   fallingObject.appendChild(fallingObjectImage);
 
   fallingObjectContainer.appendChild(fallingObject);
 
   fallingObject.addEventListener('animationend', () => {
-    // アニメーションが終了したら、現在の物を削除
     fallingObjectContainer.removeChild(fallingObject);
   });
 
   return fallingObject;
 }
 
-// 初期物の作成
 createFallingObject();
 
 rotateButton.addEventListener('click', () => {
-  // ボタンで回転
   const fallingObject = fallingObjectContainer.lastChild;
   if (fallingObject) {
     rotation += 15;
@@ -36,22 +32,15 @@ rotateButton.addEventListener('click', () => {
 });
 
 dropButton.addEventListener('click', () => {
-  // ボタンで落とす
-  const fallingObject = fallingObjectContainer.lastChild;
-  if (fallingObject) {
-    // 新しいオブジェクトを作成
-    createFallingObject();
-    try {
-      // ボタンでアニメーションを開始
-      fallingObject.style.animation = 'fall 2s linear forwards';
-    } catch (error) {
-      console.error("Error in dropButton click:", error);
-    }
+  const fallingObject = createFallingObject();
+  try {
+    fallingObject.style.animation = 'fall 2s linear forwards';
+  } catch (error) {
+    console.error("Error in dropButton click:", error);
   }
 });
 
 document.addEventListener('dblclick', () => {
-  // ダブルクリックでアニメーションをリセット
   const fallingObject = fallingObjectContainer.lastChild;
   if (fallingObject) {
     fallingObject.style.animation = '';
@@ -74,31 +63,4 @@ document.addEventListener('touchmove', (e) => {
   }
 });
 
-document.addEventListener('touchend', () => {
-  // ボタンで落とす
-  const fallingObject = fallingObjectContainer.lastChild;
-  if (fallingObject) {
-    document.addEventListener('touchend', () => {
-      // 新しいオブジェクトを作成
-      const fallingObject = createFallingObject();
-      try {
-        // ボタンでアニメーションを開始
-        fallingObject.style.animation = 'fall 2s linear forwards';
-      } catch (error) {
-        console.error("Error in touchend:", error);
-      }
-    });
-  }
-});
-
-rotateButton.addEventListener('touchstart', (e) => {
-  e.stopPropagation(); // ボタンでのタッチイベント伝播を防ぐ
-});
-
-dropButton.addEventListener('touchstart', (e) => {
-  e.stopPropagation(); // ボタンでのタッチイベント伝播を防ぐ
-});
-
-dropButton.addEventListener('dblclick', (e) => {
-  e.preventDefault(); // ダブルクリックでのデフォルトの動作を防ぐ
-});
+document.addEventListener
